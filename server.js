269914603420db6e7179a23e407b63e89e5cccfe
Austin Song <austinhssong@gmail.com>
2023-05-10 13:36:26 -0400

    Stuck at MongoDB, not reading whitelisted IP

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
 
 //routes
-app.get('/', (req, res)=>{
-  res.send('Hello Node API')
-})
+app.get("/", (req, res) => {
+  res.send("Hello Node API");
+});
 
-app.listen(3000, ()=>{
-  console.log('Node API app running on port 3000')
-})
+app.get("/blog", (req, res) => {
+  res.send("Hello blog");
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://austinhssong:MongoDBStin@devtaminyt.1zqe3af.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(3000, () => {
+      console.log("Node API app running on port 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
